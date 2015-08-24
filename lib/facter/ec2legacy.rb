7e212830ab4427d98ce81d9853feeccbe5dec77d
Chris Barker <cbarker@puppetlabs.com>
2015-08-24 10:06:37 -0700

    initial commit

---
+++ lib/facter/ec2legacy.rb
@@ -0,0 +1,29 @@
+#stolen from facter 2 util::values library
+def flatten_structure(path, structure)
+  results = {}
+
+  if structure.is_a? Hash
+    structure.each_pair do |name, value|
+    new_path = "#{path}_#{name}".gsub(/\-|\//, '_')
+    results.merge! flatten_structure(new_path, value)
+  end
+  elsif structure.is_a? Array
+    structure.each_with_index do |value, index|
+    new_path = "#{path}_#{index}"
+    results.merge! flatten_structure(new_path, value)
+  end
+  else
+    results[path] = structure
+  end
+
+  results
+end
+
+factermajversion = :facterversion.split('.')[0]
+
+if factermajversion >= 3 && (ec2_metadata = Facter.value(:ec2_metadata))
+  ec2_facts = flatten_structure("ec2", ec2_metadata)
+	ec2_facts.each_pair do |factname, factvalue|
+		Facter.add(factname, :value => factvalue)
+	end
+end
